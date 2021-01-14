a8e56f8bea3bb495744ff581fce51d64d7860ba2
Luke Parker <10430890+TheRealHona@users.noreply.github.com>
2021-01-14 15:15:58 +1000

    Drastic elo

--- JumpMapElo.Blazor/EloHelper.cs
+++ JumpMapElo.Blazor/EloHelper.cs
@@ -14,10 +14,10 @@ namespace JumpMapElo.Blazor
         {
             return 1 / (1 + Math.Pow(10, (playerTwoRating - playerOneRating) / 400.0));
         }
-        
+
         public static void CalculateElo(ref int playerOneRating, ref int playerTwoRating, Outcome outcome)
         {
-            const int eloK = 13;
+            const int eloK = 40;
             
             var delta = (int)(eloK * ((int)outcome - ExpectationToWin(playerOneRating, playerTwoRating)));
 
